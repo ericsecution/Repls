6e240c9f15297a19e91be864ba7fe36744fe445d
ericsecution <109568180+ericsecution@users.noreply.github.com>
2022-08-18 18:26:54 +0000

    adding competed Exercises

--- Main.java
+++ Main.java
@@ -1,68 +1,50 @@
 class Main {
   public static void main(String[] args) {
-  
-           int[] nums = {1,1,1,4,5,7};
-    // only added in some "[]'s" for "fun" array formatting
-            int [] result = makeMiddle(nums);
-            System.out.println();
-            System.out.println("[][][][][][][][][][][][][][][][][][][][][][][][][][][][][][]");
-            System.out.println("[][] Starting from an array of:                         [][]");
-            System.out.print("[][] ");
-              System.out.print("[START, ");
-            for (int i = 0; i < nums.length; i++) {
-                System.out.print(nums[i] + ", ");
 
-            }
-            System.out.println("END]                     [][]");
-            System.out.println("[][] And then resulting in an array of:                 [][]");
-            System.out.println("[][] [" + result[0] + ", " + result[1] + "]             "
-                                               + "                                [][]");
-            System.out.println("[][][][][][][][][][][][][][][][][][][][][][][][][][][][][][]");
-    System.out.println();
+    int a = 5;
+    int b = 7;
+    int c = 21;
 
-  
+    boolean result = lessBy10(a, b, c);
+
+    System.out.println(result);
+
+    a = 5; b = 7; c = -1;
+    result = lessBy10(a, b, c);
+    System.out.println(result);
   }
 
-/*
-Exercise 11: makeMiddle
-Create an integer array method called makeMiddle that takes in an integer array “nums”. Given an array of ints of even length, return a new array length 2 containing the middle two elements from the original array. The original array will be length 2 or more.
-makeMiddle([1, 2, 3, 4]) → [2, 3]
-makeMiddle([7, 1, 2, 3, 4, 9]) → [2, 3]
-makeMiddle([1, 2]) → [1, 2]
-*/
+  /*
+   * Exercise 14: lessBy10 Create a boolean method called lessBy10 that take in
+   * three integers, “a, “b”, and “c”. Given three ints, a b c, return true if one
+   * of them is 10 or more different than one of the others. lessBy10(1, 7, 11) →
+   * true lessBy10(1, 7, 10) → false lessBy10(11, 1, 7) → true
+   */
 
-    public static int[] makeMiddle(int[] nums) {
-  // nums[] will always have even length of integers in it
-  // the passed-in nums[] will also always be length 2 or more
-  // ex: int[] nums = {1,1,1,4,5,7}
+public static boolean lessBy10(int a, int b, int c) {
+  boolean tenOrMore = false;
 
-  // Step 1 - Determine initial variables that I'll be using
-  // -> return a new int[] -> I'll call it middleTwoArray[]
-  // -> I'll also create a left and right
-  //    int - rightMidNumber & leftMidNumber
+  // I want to create some instance variables here to use in scope
+  // so that I can compare "a -b", "a-c" & "b - c" 
+  // and put them into an array to for-loop thru
+  // Then, I just need to check for both +10 & -10
+  // and that should cover every possible combo
+  int x = a-b;
+  int y = a-c;
+  int z = b-c;
 
-        int[] middleTwoArray = new int[2];
-        
-        int rightMidNumber = 0;
-        int leftMidNumber = 0;
-
-  // Step 2 - Grab those two numbers
-  // -> I'll use .length divided by 2
-  // -> and taking that location and its +1
-
-        rightMidNumber = (nums[nums.length / 2]) ;
-        leftMidNumber = nums[nums.length / 2 - 1];
-
-  // Step 3 - instantiate the middleTwoArray with 
-  //          the two integers, left & right mid numbers
-
-        middleTwoArray[0] = leftMidNumber;
-        middleTwoArray[1] = rightMidNumber;
-
-        return middleTwoArray;
+  int[] differenceArray = new int[]{x, y, z};
+  for (int i = 0; i < differenceArray.length; i++) {
+    if(differenceArray[i] >= 10 || differenceArray[i] <= -10) {
+      tenOrMore = true;
     }
+}
+  
+  return tenOrMore;
+  
+}
 
 }
 
-//Having trouble solving the problems? After you have made a resonable effort,
-//here is a solution: https://vimeo.com/567653434
+// Having trouble solving the problems? After you have made a resonable effort,
+// here is a solution: https://vimeo.com/571850994
