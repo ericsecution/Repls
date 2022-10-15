b732ea236bd49cbe2fadfb8d6960737bab72b647
ericsecution <109568180+ericsecution@users.noreply.github.com>
2022-10-15 00:02:56 +0000

    pushing a solution to Java_Exercise-15_has23

--- Main.java
+++ Main.java
@@ -1,36 +1,38 @@
 class Main {
   public static void main(String[] args) {
 
-  int result = sumOdds();
+    int[] nums = { 0, 2 };
 
-  System.out.println("Final Result =  " + result);
+    boolean result = has23(nums);
+
+    System.out.println(result);
   }
 
-/*
-Exercise 26: sumOdds
-Create an integer method that takes in no arguments or parameters. Return the sum of the odd integers between 1 and 100 inclusive.
-  */
-// sumOdds() →  2500
+  /*
+   * Exercise 15: has23 Create a boolean method called has23 that takes in an
+   * integer array “nums”. Given an int array length 2, return true if it contains
+   * a 2 or a 3. has23([2, 5]) → true has23([4, 3]) → true has23([4, 5]) → false
+   */
 
+  public static boolean has23(int[] nums) {
+    // int[] checkNums = new int[2];
 
-public static int sumOdds() {
-int sum = 0;
-
-  for (int i =0; i < 100; i++) {
-   if (i % 2 == 1) {
-     System.out.println("");
-          System.out.println("sum: " + sum + " + ");
-     System.out.println("-> " + "i: " + i);
-     
-    sum += i;
-     System.out.println("============");
-System.out.println("Running Total = " + sum);
-     System.out.println("");
-    } 
+    for (int i = 0; i < nums.length; i++) {
+    // Scan thru array, first for a 2
+      System.out.println(nums[i]);
+    // if it finds a 2, then go into next scan for a 3
+      if (nums[i] == 2) {
+        for (int j = 0; j < nums.length - 1; j++) {
+          System.out.println(nums[j]);
+          if (nums[j] == 3) {
+          // if a 3 is found (after the 2) then return true
+            return true;
+          }
+        }
+      }
+    }
+    // otherwise (i.e. if no 2 AND 3), then return false
+    return false;
   }
-  return sum;
-
-
-}
 
 }
